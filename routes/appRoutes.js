a26fb1df46cbb8c4d09dc8770ba8b9d2356a9ae4
MohamadOjail <mohamad.ojail@gmail.com>
2022-11-11 11:30:29 +0100

    fixing modules

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { sendInstructions, testServer } from '../controller/appController.js'
 
-const router = express.Router();
+export const router = express.Router();
 
 router.get('/test', testServer);
 router.get('/info', sendInstructions);
 
-module.exports = router;
+// module.exports = router;
--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -2,8 +2,8 @@
 import express from 'express'
 import { sendMail } from '../controller/mailController.js'
 
-const router = express.Router();
+export const router = express.Router();
 
 router.post('/contact', sendMail);
 
-module.exports = router;
+// module.exports = router;
