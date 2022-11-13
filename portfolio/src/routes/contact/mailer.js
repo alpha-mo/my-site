860962bedc042a93612d7bbaf2b596b3f0964bf0
MohamadOjail <mohamad.ojail@gmail.com>
2022-11-13 22:34:13 +0100

    test

--- portfolio/src/routes/contact/mailer.js
+++ portfolio/src/routes/contact/mailer.js
@@ -1,11 +1,15 @@
 import nodemailer from "nodemailer";
 import { config } from "dotenv";
 config({ path: process.ENV })
+
+import { env } from '$env/dynamic/private';
+console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+
 import { confirmHtml, incomingHtml } from "./msg.js";
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    host: env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: true, // true for 465, false for other ports
     auth: {
